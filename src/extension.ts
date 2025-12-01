0537f2fb0db0f194ad14ee2d330a4a1a802dcb0c
konglei <lei.kong@cellwise-semi.com>
2025-12-01 17:23:57 +0800

    a

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,16 @@ import { HexReadonlyEditor } from './IntelHexViewer/hexReadonlyEditor';
 
 let myStatusBarItem: vscode.StatusBarItem;
 let genCmakesBarItem: vscode.StatusBarItem;
+let buildBarItem: vscode.StatusBarItem;
+let rebuildBarItem: vscode.StatusBarItem;
+let cmakeTerminal: vscode.Terminal | undefined;
+
+function getCMakeTerminal(): vscode.Terminal {
+	if (!cmakeTerminal || cmakeTerminal.exitStatus !== undefined) {
+		cmakeTerminal = vscode.window.createTerminal('CMake');
+	}
+	return cmakeTerminal;
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,24 +27,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposableParse = vscode.commands.registerCommand('test.parse', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		let workspaceFolders = vscode.workspace.workspaceFolders;
-		if (workspaceFolders) {
-			let inf = workspaceFolders[0];
-			vscode.window.showInformationMessage(`当前工作区目录是: ${inf.uri.fsPath}`);
-
+	// 监听终端关闭事件
+	vscode.window.onDidCloseTerminal(terminal => {
+		if (terminal === cmakeTerminal) {
+			cmakeTerminal = undefined;
 		}
-		parseTTS();
-		// const mySetting = vscode.workspace.getConfiguration().get('test.demo_setting');
-		// vscode.window.showInformationMessage('test.demo_setting : '+mySetting);
 	});
 
-	let disposableGenCMake = vscode.commands.registerCommand('test.genCMake', async () => {
+	vscode.commands.registerCommand('test.genCMake', async () => {
 		try {
 			const cmake = await generateCMakeFromConfig(context.extensionPath);
 			await writeCMakeFile(cmake);
@@ -43,20 +43,44 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(disposableParse);
-	// context.subscriptions.push(disposableGenCMake);
+	vscode.commands.registerCommand('test.cmakeConfigure', async () => {
+		const terminal = getCMakeTerminal();
+		terminal.show();
+		terminal.sendText('cmake -S . -B build -G "Ninja"');
+	});
+
+	vscode.commands.registerCommand('test.cmakeBuild', async () => {
+		const terminal = getCMakeTerminal();
+		terminal.show();
+		terminal.sendText('cmake -S . -B build -G "Ninja" && cmake --build build -j12');
+	});
+
+	vscode.commands.registerCommand('test.cmakeRebuild', async () => {
+		const terminal = getCMakeTerminal();
+		terminal.show();
+		terminal.sendText('cmake --build build --target clean && cmake -S . -B build -G "Ninja" && cmake --build build -j12');
+	});
 
 	genCmakesBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	genCmakesBarItem.text = "生成";
+	genCmakesBarItem.text = "$(file-code) Gen CMake";
 	genCmakesBarItem.command = "test.genCMake";
+	genCmakesBarItem.tooltip = "生成CMakeLists.txt";
 	context.subscriptions.push(genCmakesBarItem);
 	genCmakesBarItem.show();
 
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.text = "鱼塘建造中...";
-	myStatusBarItem.command = "test.parse";
-	context.subscriptions.push(myStatusBarItem);
-	myStatusBarItem.show();
+	buildBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 99);
+	buildBarItem.text = "$(tools) Build";
+	buildBarItem.command = "test.cmakeBuild";
+	buildBarItem.tooltip = "构建项目";
+	context.subscriptions.push(buildBarItem);
+	buildBarItem.show();
+
+	rebuildBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 98);
+	rebuildBarItem.text = "$(refresh) Rebuild";
+	rebuildBarItem.command = "test.cmakeRebuild";
+	rebuildBarItem.tooltip = "重新配置并构建";
+	context.subscriptions.push(rebuildBarItem);
+	rebuildBarItem.show();
 
 	// hex preview
 	context.subscriptions.push(HexReadonlyEditor.register(context));
